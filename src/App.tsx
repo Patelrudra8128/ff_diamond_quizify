0420a76b9d1fea6b24f23258e2612a988fd99a10
Rudra Patel <126685264+Patelrudra8128@users.noreply.github.com>
2025-11-16 17:06:38 +0530

    Update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import QuizListPage from './pages/QuizListPage';
@@ -10,39 +9,23 @@ import DetailViewPage from './pages/DetailViewPage';
 import Profile from './components/Profile';
 import ScrollToTop from './components/ScrollToTop';
 
-function AppContent() {
-  const location = useLocation();
-
-  useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-    const userId = searchParams.get('userId');
-    if (userId) {
-      localStorage.setItem('userId', userId);
-    }
-  }, [location.search]);
-
-  return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/quiz/:category" element={<QuizListPage />} />
-        <Route path="/quiz/:category/:quizIndex" element={<QuizViewPage />} />
-        <Route path="/quiz/:category/:quizIndex/result" element={<QuizResultPage />} />
-        <Route path="/details/:category" element={<DetailsListPage />} />
-        <Route path="/details/:category/:quizIndex" element={<DetailViewPage />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/profile/:userId" element={<Profile />} />
-      </Routes>
-    </Layout>
-  );
-}
-
 function App() {
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <AppContent />
-    </BrowserRouter>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/quiz/:category" element={<QuizListPage />} />
+            <Route path="/quiz/:category/:quizIndex" element={<QuizViewPage />} />
+            <Route path="/quiz/:category/:quizIndex/result" element={<QuizResultPage />} />
+            <Route path="/details/:category" element={<DetailsListPage />} />
+            <Route path="/details/:category/:quizIndex" element={<DetailViewPage />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile/:userId" element={<Profile />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
   );
 }
 
